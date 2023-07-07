84ff94df562ca685221a8f7057ca4c3c9d5cefd0
petro1909 <petya-97_97@mail.ru>
2023-07-07 09:43:00 +0300

    feat: add upadte user registration function

--- src/controller/registrationController.ts
+++ src/controller/registrationController.ts
@@ -1,25 +1,28 @@
-import { regResponseData } from 'types/registration/regRequestData';
-import { UserRepository } from '../repository/userRepository';
-import { regRequestData } from '../types/registration/regResponseData';
-import { Message } from 'types/message';
-import { createResponseMessage } from 'service/messageService';
-import { resCommandTypes } from 'types/commands/resCommandTypes';
+import { WSClientsService } from '../service/clientsService';
+import { UserService } from '../service/userService';
+import { RoomService } from '../service/roomService';
+import { Client } from '../types/entities/users';
+import { resCommandTypes } from '../types/entities/commandTypes';
+import { createResponseMessage } from '../util/messageParser';
 
 export class RegistrationController {
-  private userRepository: UserRepository;
+  private userService: UserService;
+  private roomService: RoomService;
+  private wsClientsService: WSClientsService;
+
   constructor() {
-    this.userRepository = UserRepository.getInstance();
+    this.userService = UserService.getInstance();
+    this.roomService = RoomService.getInstance();
+    this.wsClientsService = WSClientsService.getInstance();
   }
-  public userRegistration(data: string): string {
-    const registrationRequestData: regRequestData = JSON.parse(data);
-    let registrationResponseData: regResponseData;
-    try {
-      const newUser = this.userRepository.createUser(registrationRequestData.name, registrationRequestData.password);
-      registrationResponseData = { name: newUser.name, index: newUser.index, error: false, errorText: '' };
-    } catch (err) {
-      registrationResponseData = { name: '', index: 0, error: true, errorText: (err as Error).message };
-    }
-    const message: string = createResponseMessage(resCommandTypes.REGISTRATION, registrationRequestData);
-    return message;
+
+  public userRegistration(client: Client, data: string) {
+    const registrationResponseData = this.userService.userRegistration(client, data);
+    const registrationAnswerMessage: string = createResponseMessage(resCommandTypes.REGISTRATION, registrationResponseData);
+    client.wsClient.send(registrationAnswerMessage);
+
+    const roomAnswerData = this.roomService.getRooms();
+    const roomAnswerMessage = createResponseMessage(resCommandTypes.UPDATE_ROOM, roomAnswerData);
+    this.wsClientsService.getClients().forEach((client) => client.wsClient.send(roomAnswerMessage));
   }
 }
