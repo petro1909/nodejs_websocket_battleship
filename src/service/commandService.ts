54876a8cfcb495162c26e8a05490ff6d30246c3e
petro1909 <petya-97_97@mail.ru>
2023-07-08 23:10:50 +0300

    feat: add command to handle create game with bot command

--- src/service/commandService.ts
+++ src/service/commandService.ts
@@ -20,6 +20,7 @@ export class CommandService {
     this.commandHanlders.set(reqCommandTypes.REGISTRATION, this.registrationController.userRegistration.bind(this.registrationController));
     this.commandHanlders.set(reqCommandTypes.CREATE_ROOM, this.roomController.createRoom.bind(this.roomController));
     this.commandHanlders.set(reqCommandTypes.ADD_PLAYER_TO_ROOM, this.roomController.addPlayerToRoom.bind(this.roomController));
+    this.commandHanlders.set(reqCommandTypes.SINGLE_PLAY, this.gameController.createGameWithBot.bind(this.gameController));
     this.commandHanlders.set(reqCommandTypes.ADD_SHIPS, this.gameController.addShipsToBoard.bind(this.gameController));
     this.commandHanlders.set(reqCommandTypes.ATTACK, this.gameController.attack.bind(this.gameController));
     this.commandHanlders.set(reqCommandTypes.RANDOM_ATTACK, this.gameController.randomAttack.bind(this.gameController));
