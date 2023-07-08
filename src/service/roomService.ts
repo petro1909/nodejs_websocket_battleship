67f374eca3cd612442532fb182924a75c15d9b97
petro1909 <petya-97_97@mail.ru>
2023-07-08 13:33:00 +0300

    feat: update functions to manipulate room data

--- src/service/roomService.ts
+++ src/service/roomService.ts
@@ -1,5 +1,5 @@
-import { User } from '../types/entities/users';
-import { Room } from '../types/entities/room';
+import { Client, User } from '../types/entities/users';
+import { Room, RoomData } from '../types/entities/room';
 import { RoomUser } from '../types/entities/room';
 import { uuidv4 } from '../util/identification';
 import { CreateGameData } from '../types/commandsData/gameData';
@@ -15,30 +15,40 @@ export class RoomService {
     return this.instance;
   }
 
-  public getRooms(): Array<Room> {
-    return this.rooms;
+  public getRoomsData(): Array<RoomData> {
+    return this.rooms.map((room) => {
+      const roomUsers: Array<RoomUser> = [];
+      if (room.roomClients[0]) {
+        const roomUser1: RoomUser = { name: room.roomClients[0].user!.name, index: room.roomClients[0].user!.index };
+        roomUsers.push(roomUser1);
+      }
+      if (room.roomClients[1]) {
+        const roomUser2: RoomUser = { name: room.roomClients[1].user!.name, index: room.roomClients[1].user!.index };
+        roomUsers.push(roomUser2);
+      }
+      const roomData: RoomData = { roomId: room.roomId, roomUsers: roomUsers };
+      return roomData;
+    });
   }
 
-  public createRoom(creator: User): void {
+  public createRoom(creator: Client): void {
     const roomId = uuidv4();
 
-    const roomUsers: Array<RoomUser> = [];
-    const roomUser: RoomUser = { name: creator.name, index: creator.index };
-    roomUsers.push(roomUser);
+    const roomClients: Array<Client> = [];
+    roomClients.push(creator);
 
-    const room: Room = { roomId: roomId, roomUsers: roomUsers };
+    const room: Room = { roomId: roomId, roomClients: roomClients };
     this.rooms.push(room);
   }
 
-  public addPlayerToRoom(addedPlayer: User, data: string): Room {
+  public addPlayerToRoom(addedPlayerClient: Client, data: string): Room | undefined {
     const roomId = JSON.parse(data).indexRoom;
     const findedRoom = this.rooms.find((room) => room.roomId == roomId) as Room;
-    const newPlayer: RoomUser = { name: addedPlayer.name, index: addedPlayer.index };
-    findedRoom.roomUsers.push(newPlayer);
-
-    const roomIndex = this.rooms.indexOf(findedRoom);
-    this.rooms.splice(roomIndex, 1);
-
-    return findedRoom;
+    if (findedRoom.roomClients[0]?.user?.index !== addedPlayerClient.user?.index) {
+      findedRoom.roomClients.push(addedPlayerClient);
+      const roomIndex = this.rooms.indexOf(findedRoom);
+      this.rooms.splice(roomIndex, 1);
+      return findedRoom;
+    }
   }
 }
