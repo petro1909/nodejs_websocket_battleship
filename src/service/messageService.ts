5da071f28d06bbbd29cb72b359013c4095485876
petro1909 <petya-97_97@mail.ru>
2023-07-08 13:39:13 +0300

    refactor: delete unused files

--- src/service/messageService.ts
+++
@@ -1,7 +0,0 @@
-import { resCommandTypes } from 'types/commands/resCommandTypes';
-import { Message } from 'types/message';
-
-export function createResponseMessage(type: resCommandTypes, data: unknown): string {
-  const message: Message = { type: type, data: JSON.stringify(data), id: 0 };
-  return JSON.stringify(message);
-}
