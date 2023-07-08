e3dd457f2c3611d1816690503ab6d0e22940751e
petro1909 <petya-97_97@mail.ru>
2023-07-08 23:11:44 +0300

    feat: add function to create random ships array

--- src/service/gameFrameService.ts
+++ src/service/gameFrameService.ts
@@ -2,7 +2,19 @@ import { FrameCell, PlayerFrame, FrameShip } from '../types/entities/game';
 import { Ship } from '../types/entities/ship';
 
 export class GameFrameService {
-  public static getEmpyGameFrame(): Map<number, Map<number, FrameCell>> {
+  public static createPlayerFrame(ships: Array<Ship>): PlayerFrame {
+    const cellsFrame = GameFrameService.getEmptyGameFrame();
+    const playerFrame: PlayerFrame = GameFrameService.fillPlayerFrame(cellsFrame, ships);
+    return playerFrame;
+  }
+  public static createBotPlayerFrame(): PlayerFrame {
+    const ships = GameFrameService.createRandomShipsArray();
+    const cellsFrame = GameFrameService.getEmptyGameFrame();
+    const playerFrame: PlayerFrame = GameFrameService.fillPlayerFrame(cellsFrame, ships);
+    return playerFrame;
+  }
+
+  private static getEmptyGameFrame(): Map<number, Map<number, FrameCell>> {
     const frameLength = 10;
     const emptyGameFrame = new Map<number, Map<number, FrameCell>>();
     for (let i = 0; i < frameLength; i++) {
@@ -15,7 +27,7 @@ export class GameFrameService {
     return emptyGameFrame;
   }
 
-  public static fillPlayerFrame(frame: Map<number, Map<number, FrameCell>>, ships: Array<Ship>): PlayerFrame {
+  private static fillPlayerFrame(frame: Map<number, Map<number, FrameCell>>, ships: Array<Ship>): PlayerFrame {
     const frameShips: Array<FrameShip> = [];
     const shipsCount = ships.length;
     for (let i = 0; i < shipsCount; i++) {
@@ -58,5 +70,21 @@ export class GameFrameService {
       frameShips.push(frameShip);
     }
     return { ships: ships, shipsFrame: frameShips, cellsFrame: frame, liveCells: 20 };
+  }
+
+  private static createRandomShipsArray(): Array<Ship> {
+    const ships: Array<Ship> = [];
+    const ship1: Ship = { position: { x: 3, y: 7 }, direction: false, type: 'huge', length: 4 };
+    const ship2: Ship = { position: { x: 9, y: 3 }, direction: true, type: 'large', length: 3 };
+    const ship3: Ship = { position: { x: 0, y: 3 }, direction: false, type: 'large', length: 3 };
+    const ship4: Ship = { position: { x: 8, y: 0 }, direction: true, type: 'medium', length: 2 };
+    const ship5: Ship = { position: { x: 5, y: 3 }, direction: false, type: 'medium', length: 2 };
+    const ship6: Ship = { position: { x: 2, y: 0 }, direction: true, type: 'medium', length: 2 };
+    const ship7: Ship = { position: { x: 0, y: 0 }, direction: false, type: 'small', length: 1 };
+    const ship8: Ship = { position: { x: 4, y: 0 }, direction: true, type: 'small', length: 1 };
+    const ship9: Ship = { position: { x: 0, y: 8 }, direction: false, type: 'small', length: 1 };
+    const ship10: Ship = { position: { x: 8, y: 8 }, direction: false, type: 'small', length: 1 };
+    ships.push(ship1, ship2, ship3, ship4, ship5, ship6, ship7, ship8, ship9, ship10);
+    return ships;
   }
 }
