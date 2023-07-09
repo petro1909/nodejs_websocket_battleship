a963fc66d47a48e898f9b9670c2a8d7887365687
petro1909 <petya-97_97@mail.ru>
2023-07-09 16:54:12 +0300

    feat: update get winners function

--- src/service/winnerService.ts
+++ src/service/winnerService.ts
@@ -1,13 +1,13 @@
 import { Winner } from '../types/entities/users';
 import { WSClientsService } from './clientsService';
+import { UserService } from './userService';
 
 export class WinnerService {
   public static getWinners(): Array<Winner> {
-    const clients = WSClientsService.getInstance().getClients();
-    return clients
-      .filter((client) => client.user)
-      .map((client) => {
-        const winner: Winner = { name: client.user!.name, wins: client.user!.wins };
+    const users = UserService.getInstance().getUsers();
+    return users
+      .map((user) => {
+        const winner: Winner = { name: user.name, wins: user.wins };
         return winner;
       })
       .filter((winner) => winner.wins > 0);
